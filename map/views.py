d31a833a1a2193faea126f0fba53610dfdf4dd48
Ishwar Kumawar <ishwarkumawat6862@gmail.com>
2023-07-04 16:29:28 +0530

    updated map

--- map/views.py
+++ map/views.py
@@ -4,27 +4,13 @@ from django.core import serializers
 import json
 
 
+
 def map_view(request):
     # Fetch incidents data from your model or database
     incidents = Incident.objects.all()
     incidents_data = [{'latitude': incident.location.y, 'longitude': incident.location.x} for incident in incidents]
-    incidents_json = json.dumps(incidents_data)
-
-    # Serialize the incidents data to JSON
-    # incidents_json = serializers.serialize('json', incidents)
-
-#     incidents_data = json.dumps([
-#     {
-#         'latitude': incident.location[0],
-#         'longitude': incident.location[1],
-#     }
-#     for incident in incidents
-# ])
-
-# Pass the incidents_data to the context or render it to the template
+    
     context = {
-        'incidents': incidents_json
+        'incidents': json.dumps(incidents_data)  # Serialize incidents_data as JSON
     }
-    print(incidents[0].location[0])
-    print(incidents[0].location[1])
     return render(request, 'map/map.html', context)
--- static/map.js
+++ static/map.js
@@ -1,12 +1,30 @@
+// Initialize the map
 function initMap() {
   var mapOptions = {
-      center: { lat: 25.2623, lng: 82.9893 },
-      zoom: 16,
-      scrollwheel: true // Enable zooming on mouse scrolling
+    center: { lat: 25.2623, lng: 82.9893 },
+    zoom: 16,
+    scrollwheel: true // Enable zooming on mouse scrolling
   };
   map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-  var myDiv = document.getElementById('myDiv');
-  var incidentsData = JSON.parse(myDiv.getAttribute('data-mydata'));
-  console.log(incidentsData);
+  // Loop through the incidents data and draw circles
+  var incidents = JSON.parse('{{ incidents|safe }}');
+
+  for (var i = 0; i < incidents.length; i++) {
+    var incident = incidents[i];
+    var incidentLocation = new google.maps.LatLng(incident.latitude, incident.longitude);
+    var incidentCircle = new google.maps.Circle({
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.2,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+      map: map,
+      center: incidentLocation,
+      radius: 50,
+      circleOptions: {
+        numberOfPoints: 40 // Adjust the number of points for smoother boundaries
+      }
+    });
+  }
 }
